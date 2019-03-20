e9b61b57e7f9e7a3d03f20e9c03b91d748e8e5cc
Kenia Rioja <kdrioja@gmail.com>
2019-03-19 21:07:49 -0700

    Working on Interval Scheduling

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Interval> intervals = CreateIntervals.getUserInput();
 
+        System.out.println("==========Interval Intervals==========");
         for (int i = 0; i < intervals.size(); i++) {
             System.out.println(intervals.get(i).toString());
         }
+
+        System.out.println("==========Interval Scheduling==========");
+        
     }
 }
