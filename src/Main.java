54fe4d8d9bbd442cd57137a8aa7609a02d67eb5a
Kenia Rioja <kdrioja@gmail.com>
2019-03-20 23:43:19 -0700

    Working on Interval Scheduling

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ public class Main {
         }
 
         System.out.println("==========Interval Scheduling==========");
-        
+
+
+        System.out.println("==========Partition Scheduling==========");
     }
 }
