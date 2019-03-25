9eddbf7213d8d5f26c8ea9fbb10e299cb193b60a
Kenia Rioja <kdrioja@gmail.com>
2019-03-24 18:23:07 -0700

    Working on HeapSort class

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -6,16 +6,48 @@ public class HeapSort {
      * Sorts an array in place
      * @param A
      */
-    public static void heapsort(Array[] A) {
-
+    public static void heapsort(int[] A) {
+        buildMaxHeap(A);
+        for (int i = A.length; i > 1; i--) {
+            
+        }
     }
 
     /**
      * O(log n)
      * Key to maintaining the max-heap property
      */
-    public static void maxHeapify(Interval[] A, int i) {
+    //parent floor of i/2
+    //left 2i
+    //right 2i + 1
+    //but these start from 0 so
+    //left 2i + 1
+    //right 2i + 2
+    public static void maxHeapify(int[] A, int i) {
+        //these are all indexes
+        int largest = i;
+        int left = (2*i) + 1;
+        int right = (2*i) + 2;
 
+        if (left < A.length && A[left] > A[i]) {
+            largest = left;
+        }
+        if (right < A.length && A[right] > A[i]) {
+            largest = right;
+        }
+        if (largest != i) {
+            //exchange A[i] with A[largest]
+            int temp = A[i];
+            A[i] = A[largest];
+            A[largest] = temp;
+            maxHeapify(A, largest);
+        }
+    }
+
+    public static void buildMaxHeap(int[] A) {
+        for (int i = A.length/2; i > -1; i--) {
+            maxHeapify(A, i);
+        }
     }
 
 }
