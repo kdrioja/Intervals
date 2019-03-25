284dc787a1391f21289a24b39320b220bd1829c4
Kenia Rioja <kdrioja@gmail.com>
2019-03-24 20:41:41 -0700

    Implemented minSort and maxSort

--- src/HeapSort.java
+++ src/HeapSort.java
@@ -1,38 +1,41 @@
-import java.sql.Array;
-
 public class HeapSort {
-    /**
-     * O(log n)
-     * Sorts an array in place
-     * @param A
-     */
-    public static void heapsort(int[] A) {
-        buildMaxHeap(A);
-        for (int i = A.length; i > 1; i--) {
-            
+
+    public static void maxSort(int A[]) {
+        int heapSize = A.length;
+
+        buildMaxHeap(A, heapSize);
+
+        for (int i = heapSize - 1; i > -1; i--) {
+            int temp = A[0];
+            A[0] = A[i];
+            A[i] = temp;
+            maxHeapify(A, 0, i);
         }
     }
 
-    /**
-     * O(log n)
-     * Key to maintaining the max-heap property
-     */
-    //parent floor of i/2
-    //left 2i
-    //right 2i + 1
-    //but these start from 0 so
-    //left 2i + 1
-    //right 2i + 2
-    public static void maxHeapify(int[] A, int i) {
+    public static void minSort(int A[]) {
+        int heapSize = A.length;
+
+        buildMinHeap(A, heapSize);
+
+        for (int i = heapSize - 1; i > -1; i--) {
+            int temp = A[0];
+            A[0] = A[i];
+            A[i] = temp;
+            minHeapify(A, 0, i);
+        }
+    }
+
+    public static void maxHeapify(int A[], int i, int heapSize) {
         //these are all indexes
         int largest = i;
-        int left = (2*i) + 1;
-        int right = (2*i) + 2;
+        int left = (2 * i) + 1;
+        int right = (2 * i) + 2;
 
-        if (left < A.length && A[left] > A[i]) {
+        if (left < heapSize && A[left] > A[largest]) {
             largest = left;
         }
-        if (right < A.length && A[right] > A[i]) {
+        if (right < heapSize && A[right] > A[largest]) {
             largest = right;
         }
         if (largest != i) {
@@ -40,14 +43,39 @@ public class HeapSort {
             int temp = A[i];
             A[i] = A[largest];
             A[largest] = temp;
-            maxHeapify(A, largest);
+            maxHeapify(A, largest, heapSize);
         }
     }
 
-    public static void buildMaxHeap(int[] A) {
-        for (int i = A.length/2; i > -1; i--) {
-            maxHeapify(A, i);
+    public static void minHeapify(int A[], int i, int heapSize) {
+        int smallest = i;
+        int left = (2 * i) + 1;
+        int right = (2 * i) + 2;
+
+        if (left < heapSize && A[left] < A[smallest]) {
+            smallest = left;
+        }
+        if (right < heapSize && A[right] < A[smallest]) {
+            smallest = right;
+        }
+        if (smallest != i) {
+            //exchange A[i] with A[largest]
+            int temp = A[i];
+            A[i] = A[smallest];
+            A[smallest] = temp;
+            minHeapify(A, smallest, heapSize);
         }
     }
 
+    public static void buildMaxHeap(int A[], int heapSize) {
+        for (int i = (heapSize / 2) - 1; i > -1; i--) {
+            maxHeapify(A, i, heapSize);
+        }
+    }
+
+    public static void buildMinHeap(int A[], int heapSize) {
+        for (int i = (heapSize / 2) - 1; i >= 0; i--) {
+            minHeapify(A, i, heapSize);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        /**
         ArrayList<Interval> intervals = CreateIntervals.getUserInput();
 
         System.out.println("==========Interval Intervals==========");
@@ -14,5 +15,22 @@ public class Main {
 
 
         System.out.println("==========Interval Partitioning==========");
+
+        */
+        int[] a = {9, 8, 4, 5, 3, 1, 7, 6, 2, 0, 11, 22};
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        HeapSort.maxSort(a);
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        HeapSort.minSort(a);
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+
     }
 }
